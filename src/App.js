0911a562dcccc50710a24a942f647384ec2db14a
GaaneshT <dstworstsubject@gmail.com>
2020-07-20 17:31:50 +0800

    Changes for v2

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          SEND NUDES PLS. WE IS FAILING
         </p>
         <a
           className="App-link"
