01bf05acb60dadcb999a6e70c2e157599a69cc75
GaaneshT <dstworstsubject@gmail.com>
2020-07-21 19:18:10 +0800

    Changes for SuppBotV3

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
   render() {
     return (
       <div style={ { height: '100vh' } }>
-        <SumerianScene sceneName='SuppBotScene'/>
+        <SumerianScene sceneName='TestUS1'/>
       </div>
     );
   }
